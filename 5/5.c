19c2a2089f68ab00388965ee1aac42ba1b7f49cf
Ura2 <urauraminecraft2529@gmail.com>
2025-01-13 02:06:10 +0900

    Add/Update changes

--- 5/5.c
+++ 5/5.c
@@ -1,28 +1,30 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-int main (){
-    int t = 0; // 変数tを初期化
-    double f = 0.7; // 変数fを0.7で初期化
-    int n = 1; // 変数nを初期化
+int main() {
+    int t = 0; // 変数 t の初期化
+    double f = 0.7; // 初期値 f の設定
+    int n = 1; // 変数 n の初期化
+    
+    double lower_bound = pow(2, -n);   // 下限の計算
+    double upper_bound = pow(2, -n+1); // 上限の計算
 
-    double lower_bound = pow(2, -n);
-    double upper_bound = pow(2, -n+1);
+    printf("%lf → %lf\n", lower_bound, upper_bound); // 下限と上限の出力
 
-    printf("f = %lf\n", f); // 変数fの値を出力
-    printf("lower_bound = %.10lf\n", lower_bound); // lower_boundの値を出力
-    printf("upper_bound = %.10lf\n", upper_bound); // upper_boundの値を出力
+    // 1 から 100 までのループ
+    for (t = 1; t <= 100; t++) {
+        
 
-    // 1から100までのループ
-    for(t = 1; t <= 100; t++){
-        // fが0以上で2の-n乗未満の場合
-        if(f >= 0 && f < lower_bound) {
-            printf("0<=f<%.1lf: %.10lf\n", lower_bound, pow(2, n) * f); // 2のn乗とfの積を出力
-        // fが2の-n乗以上で2の-n+1乗以下の場合
-        } else if(f >= lower_bound && f <= upper_bound) {
-            printf("%.1lf<=f<=%.1lf: %.10lf\n", lower_bound, upper_bound, -pow(2, n) * (f - lower_bound)); // 計算結果を出力
-        } else{
-            printf("else: %.10lf\n", f - pow(2, -n+1)); // 計算結果を出力
+        // 条件に基づく計算と出力
+        if (f >= 0 && f < lower_bound) {
+            f = pow(2, n) * f; // f の計算
+            printf("f = %.10lf\n", f); // f の出力
+        } else if (f >= lower_bound && f <= upper_bound) {
+            f = -pow(2, n) * (f - lower_bound); // f の計算
+            printf("f = %.10lf\n", f); // f の出力
+        } else if (f < 0 || f > upper_bound) {
+            f = f - upper_bound; // f の計算
+            printf("f = %.10lf\n", f); // f の出力
         }
     }
 

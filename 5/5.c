7c069ca7541d29758c286f8d489506bd59521286
Ura2 <urauraminecraft2529@gmail.com>
2025-01-15 14:56:24 +0900

    a

--- 5/5.c
+++ 5/5.c
@@ -3,30 +3,31 @@
 
 int main() {
     int t = 0; // 変数 t の初期化
-    double f = 0.7; // 初期値 f の設定
+    double f = 0.4; // 初期値 f の設定
     int n = 1; // 変数 n の初期化
     
     double lower_bound = pow(2, -n);   // 下限の計算
     double upper_bound = pow(2, -n+1); // 上限の計算
 
-    printf("%lf → %lf\n", lower_bound, upper_bound); // 下限と上限の出力
-
-    // 1 から 100 までのループ
-    for (t = 1; t <= 100; t++) {
-        
+    // printf("下限%lf → 上限%lf\n", lower_bound, upper_bound); // 下限と上限の出力
+    printf("%f <= f(t) < %f → %ff(t)\n", 0, lower_bound,pow(2, n)); 
+    printf("%f <= f(t)<= %f → %f(f(t)%f)\n", lower_bound, upper_bound,-pow(2, n),-pow(2, -n+1));
+    // 0 から 99 までのループ
+    for (t = 0; t <= 99; t++) {
+        printf("%d:",t); // t の出力
 
         // 条件に基づく計算と出力
         if (f >= 0 && f < lower_bound) {//0 <= f < 0.5
             f = pow(2, n) * f; 
-            printf("f = %.10lf\n", f); 
+            printf(" %.10lf\n", f); 
 
         } else if (f >= lower_bound && f <= upper_bound) {// 0.5 <= f <= 1
             f = -pow(2, n) * (f - pow(2, -n+1)); 
-            printf("f = %.10lf\n", f); 
+            printf(" %.10lf\n", f); 
 
         } else if (f < 0 || f > upper_bound) {// 範囲外の場合
             f = f - upper_bound; 
-            printf("f = %.10lf\n", f); 
+            printf("%.10lf\n", f); 
         }
     }
 
